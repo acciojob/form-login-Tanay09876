104e96769dc890f28ba4c52c6fac9acff0bdc76b
Tanay09876 <152384332+Tanay09876@users.noreply.github.com>
2025-06-17 13:54:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
-function getFormvalue() {
-    //Write your code here
 
-}
+ function getFormvalue(event) {
+      event.preventDefault(); // Prevents page reload on form submit
+
+      const form1 = event.target;
+      const fname = form1.fname.value.trim();
+      const lname = form1.lname.value.trim();
+
+      const fullName = fname + " " + lname;
+
+      alert(fullName);
+    }
